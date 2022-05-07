49ac63b18e4b3e8a2203d4c8be8e48c9b17eb3ff
SouravBiswas238 <souravbiswas427@gmail.com>
2022-05-07 08:54:56 +0600

    add data

--- index.js
+++ index.js
@@ -1,26 +1,49 @@
-const express = require('express')
-const app = express()
-const port = 5000;
-
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
 const { MongoClient, ServerApiVersion } = require('mongodb');
+
+const port = process.env.PORT || 5000;
+const app = express();
+
+// middlewirw
+app.use(cors());
+app.use(express.json());
 
 // yuP8WwAjKkTBlKU6
 // bike-directory
 
-
-
-
-const uri = "mongodb+srv://bike-directory:yuP8WwAjKkTBlKU6@cluster0.f3b8d.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.f3b8d.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 
 
-client.connect(err => {
-    console.log('insert from mongo')
-    const collection = client.db("test").collection("devices");
-    // perform actions on the collection object
-    // client.close();
-});
+async function run() {
+    try {
+        await client.connect();
+        const bikeCollection = client.db("bikes-collection").collection("bikes");
+        // to redad
+
+        console.log('connected to db');
+
+        app.get('/bikes', async (req, res) => {
+            const query = {};
+
+            const cursor = bikeCollection.find(query)
+            const bikes = await cursor.toArray();
+
+            res.send(bikes);
+        })
+
+        // create bikes information
+
+        // update 
+    }
+    finally {
+
+    }
+}
+run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
